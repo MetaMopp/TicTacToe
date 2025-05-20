4d408f1e3d9d5e46539f402d2adac981ef2504f7
MetaMopp <metamopp@gmail.com>
2025-05-20 11:05:21 +0200

    before: patt

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ from objects import Board
 from objects import HumanPlayer
 from objects import ComputerPlayer
 from pyfiglet import Figlet
+from time import sleep
 
 
 class Game:
@@ -10,41 +11,63 @@ class Game:
         self.board = Board()
         self.human = HumanPlayer("O")
         self.computer = ComputerPlayer("X")
+        self.winner = None
 
     def welcome(self):
         ttt_font = Figlet(font='big')
         print(''' 
-        / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \_
-       / /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \__
-       \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / _
-        \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ / 
+ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \_
+/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \__
+\ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / _
+ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ / 
         ''')
-        print(ttt_font.renderText('        TIC TAC TOE') , end="")
+        print(ttt_font.renderText(' TIC TAC TOE') , end="")
         print('''
-                        ||================================||
-                        ||<> < metamopp[at]gmail.com > <> ||           
-                        ||================================||
+                ||================================||
+                ||<> < metamopp[at]gmail.com > <> ||           
+                ||================================||
         ''')
         print('''
-        / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \_
-       / /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \__
-       \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / _
-        \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ / 
+ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \_
+/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \__
+\ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / _
+ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ / 
         ''')
+
+    def declare_victory(self):
+        print(f"{self.winner} won!")
+        sleep(3)
 
     def run(self):
         # display
-        self.board.display()
+        #self.board.display()
         if "X" not in self.board.fields:
+            self.board.display()
+            print("Computer's sign: 'X'")
+            print("Your sign: 'O'")
+            print()
             self.computer.first_move()
         else:
             self.computer.choose_move(self.board)
         self.board.update(self.computer.move, self.computer.sign)
         self.board.display()
-        self.board.check_victory()
-        self.human.choose_move(self.board)
-        self.board.update(self.human.move, self.human.sign)
-        self.board.display()
+        (self.victory, self.winner) = self.board.check_victory(self.computer.sign)
+        #self.victory, self.winning = result[0], result[1]
+        if self.victory:
+            self.declare_victory()
+            self.running = False
+        if self.running == True:
+            self.human.choose_move(self.board)
+            self.board.update(self.human.move, self.human.sign)
+            self.board.display()
+            (self.victory, self.winner) = self.board.check_victory(self.human.sign)
+            if self.victory:
+                self.declare_victory()
+                self.running = False
+
+        
+            
+
 
 
 
--- mainloop.py
+++ mainloop.py
@@ -1,31 +1,7 @@
 from game import Game
 from time import sleep
-from pyfiglet import Figlet
-
-ttt_font = Figlet(font='big')
 
 
-print(''' 
- / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \_
-/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \__
-\ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / _
- \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ / 
- ''')
-print(ttt_font.renderText('TIC TAC TOE') , end="")
-
-print('''
-                ||================================||
-                ||<> < metamopp[at]gmail.com > <> ||           
-                ||================================||
-''')
-
-print('''
- \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ / 
- / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \_
-/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \__
-\ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / __ \ \__/ / _
- \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ /  \ \____/ / 
-''')
 
 game = Game()
 game.welcome()
@@ -34,7 +10,12 @@ game.welcome()
 while game.running:
     game.run()
 
-if game.running == False:
-    print(game.board.victory_message)
-    sleep(3)
-    print("You you want to start a new Game?")
+    if game.running == False:
+        print("running: ",game.running)
+        sleep(3)
+        choice = input("You you want to start a new Game? y/n ")
+        if choice != "y":
+            exit()
+        else:
+            game.running == True
+
